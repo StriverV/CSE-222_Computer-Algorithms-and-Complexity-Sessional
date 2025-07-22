73822613d08c11f46e229015046e9029de6be2f9
Striver <187439614+nosratee-jahan-naba@users.noreply.github.com>
2025-07-22 10:05:05 +0600

    Update Bubble_sort.cpp

--- Lab 1/Bubble_sort.cpp
+++ Lab 1/Bubble_sort.cpp
@@ -1,41 +1,54 @@
 #include<bits/stdc++.h>
 #define Naba ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
 using namespace std;
+vector<int>v(1005);
 
-
-void BubbleSort(vector<int>& v)     // arr-> int a[]
+void print(int n)
 {
-    int n = v.size();
-    for(int i = 0; i < n - 1; i++)
+    for(int i = 0; i < n; i++)
+        cout << v[i] << " ";
+}
+void BubbleSort(int n)
+{
+    int cnt = 0;
+    for(int i = 0; i < n-1; i++)
     {
         bool swapped = false;
-
-        for(int j = 0; j < n - i - 1; j++)
+        cout <<"step " << i+1 <<" : ";
+        for(int j = 0; j < n-i-1; j++)
         {
-            if(v[j] > v[j + 1])
+            if(v[j] > v[j+1])
             {
-                swap(v[j], v[j + 1]);
-                swapped = true;
+                swap(v[j], v[j+1]);
+                swapped=true;
+                cnt++;
             }
+                
         }
-
+        print(n);
+        cout<<endl;
+      
         if(!swapped)
+        {
+            cout << "No swapped happen" << endl;
             break;
+        }
+            
     }
+    cout << "Total swapped : " << cnt << endl;
+    print(n);
 }
 
 int main()
 {
     Naba;
-    int n; cin >> n;
-    vector<int> v(n);
-    for(int i = 0; i < v.size(); i++)
-        cin >> v[i];
+    int n;
+    cin >> n;
     
-    BubbleSort(v);
-
-    for(int i = 0; i < v.size(); i++)
-        cout << v[i] << " ";
-
+    for(int i = 0; i < n; i++)
+       cin >> v[i];
+       
+    BubbleSort(n);
+    
     return 0;
 }
