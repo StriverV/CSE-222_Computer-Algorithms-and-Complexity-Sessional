6ac515743f0f72ad63a3b53c7847adb564733c27
Striver <187439614+nosratee-jahan-naba@users.noreply.github.com>
2025-07-22 21:09:29 +0600

    Update SelectionSort.cpp

--- Lab 1/SelectionSort.cpp
+++ Lab 1/SelectionSort.cpp
@@ -1,40 +1,51 @@
 #include<bits/stdc++.h>
-#define Naba ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+using namespace std;
 
-void selectionSort(int a[], int n)
+vector<int>v(1005);
+
+void print(int n)
 {
-    for (int i = 0; i < n - 1; i++) 
+    for(int i = 0; i < n; i++)
     {
-        int min = i;
-
-        for (int j = i + 1; j < n; j++)
-        {
-            if (a[j] < a[min]) 
-                min = j;
-            
-        }
-
-        if (min != i) 
-            swap(a[i], a[min]);
-        
+        cout << v[i] << " ";
     }
+    cout << endl;
 }
 
-int main() 
+void SelectionSort(int n)
 {
-    Naba;
+    int cnt = 0;
+    for(int i = 0; i < n-1; i++)
+    {
+        int min_idx = i;
+        for(int j = i+1; j < n; j++)
+        {
+            if(v[min_idx] > v[j])
+            {
+                min_idx = j;
+            }   
+        }
+        if(min_idx != i)
+        {
+            swap(v[i], v[min_idx]);
+            cnt++;
+        }
+        cout << "step " << i+1 <<" : ";
+        print(n);
+    }
+    cout <<"Total swaps: " << cnt << endl;
+}
+
+int main()
+{
+    int n; cin >> n;
     
-    int n;  cin >> n;
-    int a[n];
-    for (int i = 0; i < n; i++) 
-        cin >> a[i];
+    for(int i = 0; i < n; i++)
+        cin >> v[i];
     
-
-    selectionSort(a, n);
-
-    for (int i = 0; i < n; i++) 
-        cout << a[i] << " ";
-
+    print(n);
+    SelectionSort(n);
+    print(n);
 
     return 0;
 }
