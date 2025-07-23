bff695ff4a7eabd693bb4eb0be7863527b313577
Striver <187439614+nosratee-jahan-naba@users.noreply.github.com>
2025-07-23 07:48:42 +0600

    Update Fractional_Knapsack_Greedy.cpp

--- Lab 4/Fractional_Knapsack_Greedy.cpp
+++ Lab 4/Fractional_Knapsack_Greedy.cpp
@@ -1,39 +1,37 @@
 #include<bits/stdc++.h>
 using namespace std;
-
-double v[100];
-double w[100];
+vector<double>v(100);
+vector<double>w(100);
 int item_idx[100];
 
 bool cmp(int i, int j)
 {
-    return ( (v[i]/w[i])  >  (v[j]/w[j]));
+    return ((v[i]/w[i]) > v[j]/w[j]);
 }
 
-double fractionalKnapsack(int n, int c)
+double fk(int n, int c)
 {
-    for(int i = 0; i < n; i++)   
-        item_idx[i] = i;
+    for(int i = 0; i < n; i++)  item_idx[i] = i;
 
-    sort(item_idx, item_idx + n, cmp);
+    sort(item_idx, item_idx+n, cmp);
+    double total = 0;
 
-    double total = 0.0;
-    
     for(int i = 0; i < n; i++)
     {
-        int itemNo = item_idx[i];
-        if(w[itemNo] <= c)
+        int item_no = item_idx[i];
+
+        if(w[item_no] <= c)
         {
-            total += v[itemNo];
-            c -= w[itemNo];
+            total += v[item_no];
+            c -= w[item_no];
         }
         else
         {
-            total += (v[itemNo]/w[itemNo]) * c;
+            total += (v[item_no]/w[item_no]) * c;
             break;
         }
     }
-
+    
     return total;
 }
 
@@ -41,11 +39,11 @@ int main()
 {
     int n, c;
     cin >> n >> c;
-    
-    for(int i = 0; i < n; i++)  cin >> v[i];
-    for(int i = 0; i < n; i++)  cin >> w[i];
 
-    cout << fractionalKnapsack(n, c) << endl;
+    for(int i = 0; i < n; i++)   cin >> v[i];
+    for(int i = 0; i < n; i++)   cin >> w[i];
+
+    cout << fixed << setprecision(2) <<fk(n, c) << endl;
 
     return 0;
 }
